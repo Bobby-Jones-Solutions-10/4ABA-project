77aa0f6f36bb70b78744db8c0fe96ec6c884207a
Tiago <tiagoepitanga10@gmail.com>
2023-08-17 12:58:10 -0300

    update

--- src/components/ServicesPosts/index.tsx
+++ src/components/ServicesPosts/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function ServicesPosts({ title, content, id, date, local }: ServicesPostsProps) {
     return (
         <>
-            <div className='flex flex-col gap-4 border-2 w-3/12 p-2 m-2 h-full shadow-lg items-start justify-start'>
+            <div key={id} className='flex flex-col gap-4 border-2 w-3/12 p-2 m-2 h-full shadow-lg items-start justify-start'>
                 <div className='flex '>
                     <Link href={`services/${title}`}>
                         {title}
