ce4ba9780712124e8872f687e373b4b6a7b15a93
Tiago <tiagoepitanga10@gmail.com>
2023-08-17 13:04:48 -0300

    update

--- next.config.js
+++ next.config.js
@@ -5,15 +5,15 @@ const nextConfig = {
 
     trailingSlash: true, //insere uma barra no final de todo link 
     output: 'export', //exporta o projeto para uma pasta chamada export
-    async redirects() {
-        return [
-            {
-                source: '/casa', // pagina que sera digitada no navegador
-                destination: '/home', // pagina que sera redirecioda
-                permanent: true,
-            },
-        ]
-    }
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: '/casa', // pagina que sera digitada no navegador
+    //             destination: '/home', // pagina que sera redirecioda
+    //             permanent: true,
+    //         },
+    //     ]
+    // }
 }
 
 module.exports = nextConfig
