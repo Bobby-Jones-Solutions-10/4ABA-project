65b94000ffcd7ce04e641001e8f94f0f05e7361a
Tiago <tiagoepitanga10@gmail.com>
2023-09-11 13:14:34 -0300

    reestruturando pastas

--- src/app/(userRoutes)/layout.js
+++ src/app/(userRoutes)/layout.js
@@ -1,5 +1,6 @@
+import { nextAuthOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
-import { redirect } from "next/dist/server/api-utils";
+import { redirect } from "next/navigation";
 
 export default async function PrivateLayout({ children }) {
   const session = await getServerSession(nextAuthOptions)
